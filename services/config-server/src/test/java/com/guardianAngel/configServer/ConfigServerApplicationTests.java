6a2c784436caf6d56d66f3285ccae0d4b57ee2f6
Pedro Maluco <phtbarreto@gmail.com>
2026-01-22 11:22:28 -0300

    initial commit

---
+++ services/config-server/src/test/java/com/guardianAngel/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guardianAngel.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
