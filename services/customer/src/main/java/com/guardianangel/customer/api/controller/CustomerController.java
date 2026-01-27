798abcf708018ca398ccbdd82e91357b8dcdfc91
Pedro Maluco <phtbarreto@gmail.com>
2026-01-27 06:42:20 -0300

    for some reason, postman cannot find my api for customer. Needs fix

--- services/customer/src/main/java/com/guardianangel/customer/api/controller/CustomerController.java
+++ services/customer/src/main/java/com/guardianangel/customer/api/controller/CustomerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(name = "/v1/api/customer")
+@RequestMapping("/v1/api/customer")
 @RequiredArgsConstructor
 public class CustomerController {
 
